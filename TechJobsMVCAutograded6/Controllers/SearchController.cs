2c0017cb2eda1b4538dbd83c3c9a67afcb514b3a
Tiara <tburtin1019@gmail.com>
2023-04-10 16:49:47 -0500

    attempts at 2-4

--- TechJobsMVCAutograded6/Controllers/SearchController.cs
+++ TechJobsMVCAutograded6/Controllers/SearchController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TechJobsMVCAutograded6.Data;
@@ -18,5 +19,24 @@ public class SearchController : Controller
     }
 
     // TODO #3 - Create an action method to process a search request and render the updated search views.
+    public IActionResult Results(string searchType,string searchTerm)
+    { 
+        List<Job> jobs;
+        {
+            if (searchTerm.ToLower().Equals("all"))
+            {
+                jobs = JobData.FindAll();
+            } else if (searchTerm.ToLower().Equals(""))
+                {
+                jobs = JobData.FindAll();
+            } else
+            {
+                JobData.FindByColumnAndValue(searchType,searchTerm);
+                jobs = JobData.FindByColumnAndValue();
+            }
+        }
+        return View(Results);
+    }
+    
 }
 
